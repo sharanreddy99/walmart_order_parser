0c88579a2a93807e6ab075744e2c688cfefb4450
Sharan Reddy <sharansaikonda99@gmail.com>
2023-11-08 13:15:13 -0500

    fix responsiveness

--- frontend/src/components/Summary/Summary.jsx
+++ frontend/src/components/Summary/Summary.jsx
@@ -32,37 +32,99 @@ const Summary = ({ orderDetails, groups }) => {
   };
 
   return (
-    <Container maxWidth={false}>
-      <Grid container>
-        <Grid xs={12} item>
-          <Card
-            sx={{
-              float: "left",
-              margin: "1%",
-              maxWidth: 460,
-            }}
-            key={"Order Summary"}
-          >
-            <CardContent>
-              <Typography
-                sx={{
-                  fontSize: 14,
-                  fontWeight: "bold",
-                  textTransform: "uppercase",
-                }}
-                gutterBottom
-              >
-                Order Summary
-              </Typography>
-              <TableContainer component={Paper}>
-                <Table sx={{ maxWidth: 450 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow className="table_header">
-                      <TableCell className="table_header_cell">Field</TableCell>
-                      <TableCell className="table_header_cell">Value</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
+    <Grid container alignItems="stretch">
+      <Grid item style={{ display: "flex" }} xs={4}>
+        <Card
+          sx={{
+            float: "left",
+            margin: "1%",
+            width: "100%",
+          }}
+          key={"Order Summary"}
+        >
+          <CardContent>
+            <Typography
+              sx={{
+                fontSize: 14,
+                fontWeight: "bold",
+                textTransform: "uppercase",
+              }}
+              gutterBottom
+            >
+              Order Summary
+            </Typography>
+            <TableContainer component={Paper}>
+              <Table sx={{ width: "100%" }} aria-label="simple table">
+                <TableHead>
+                  <TableRow className="table_header">
+                    <TableCell className="table_header_cell">Field</TableCell>
+                    <TableCell className="table_header_cell">Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>
+                      Sub Total (Excluding all taxes and fees)
+                    </TableCell>
+                    <TableCell>{orderDetails.subTotal || 0}</TableCell>
+                  </TableRow>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>Tax</TableCell>
+                    <TableCell>{orderDetails.tax || 0}</TableCell>
+                  </TableRow>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>Delivery Fee</TableCell>
+                    <TableCell>{orderDetails.deliveryFee || 0}</TableCell>
+                  </TableRow>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>Delivery Tip</TableCell>
+                    <TableCell>{orderDetails.tip || 0}</TableCell>
+                  </TableRow>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>Order Total</TableCell>
+                    <TableCell>{orderDetails.total || 0}</TableCell>
+                  </TableRow>
+                  <TableRow
+                    className="table_body"
+                    key={"table_summary_row1"}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                    }}
+                  >
+                    <TableCell>Split Total</TableCell>
+                    <TableCell>{getTotalGroupsContribution() || 0}</TableCell>
+                  </TableRow>
+                  {orderDetails.total - getTotalGroupsContribution() > 0 ? (
                     <TableRow
                       className="table_body"
                       key={"table_summary_row1"}
@@ -70,170 +132,103 @@ const Summary = ({ orderDetails, groups }) => {
                         "&:last-child td, &:last-child th": { border: 0 },
                       }}
                     >
-                      <TableCell>
-                        Sub Total (Excluding all taxes and fees)
+                      <TableCell>Remaining</TableCell>
+                      <TableCell sx={{ fontWeight: "bold" }}>
+                        {(
+                          orderDetails.total - getTotalGroupsContribution()
+                        ).toFixed(4)}
                       </TableCell>
-                      <TableCell>{orderDetails.subTotal || 0}</TableCell>
                     </TableRow>
-                    <TableRow
-                      className="table_body"
-                      key={"table_summary_row1"}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>Tax</TableCell>
-                      <TableCell>{orderDetails.tax || 0}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      className="table_body"
-                      key={"table_summary_row1"}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>Delivery Fee</TableCell>
-                      <TableCell>{orderDetails.deliveryFee || 0}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      className="table_body"
-                      key={"table_summary_row1"}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>Delivery Tip</TableCell>
-                      <TableCell>{orderDetails.tip || 0}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      className="table_body"
-                      key={"table_summary_row1"}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>Order Total</TableCell>
-                      <TableCell>{orderDetails.total || 0}</TableCell>
-                    </TableRow>
-                    <TableRow
-                      className="table_body"
-                      key={"table_summary_row1"}
-                      sx={{
-                        "&:last-child td, &:last-child th": { border: 0 },
-                      }}
-                    >
-                      <TableCell>Split Total</TableCell>
-                      <TableCell>{getTotalGroupsContribution() || 0}</TableCell>
-                    </TableRow>
-                    {orderDetails.total - getTotalGroupsContribution() > 0 ? (
-                      <TableRow
-                        className="table_body"
-                        key={"table_summary_row1"}
-                        sx={{
-                          "&:last-child td, &:last-child th": { border: 0 },
-                        }}
-                      >
-                        <TableCell>Remaining</TableCell>
-                        <TableCell sx={{ fontWeight: "bold" }}>
-                          {(
-                            orderDetails.total - getTotalGroupsContribution()
-                          ).toFixed(4)}
+                  ) : null}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+        </Card>
+      </Grid>
+      {Object.keys(groups).map((groupName) => {
+        return (
+          <Grid key={groupName} xs={4} sx={{ display: "flex" }}>
+            <Card
+              sx={{
+                float: "left",
+                margin: "1%",
+                width: "100%",
+              }}
+            >
+              <CardContent>
+                <Typography
+                  sx={{
+                    fontSize: 14,
+                    fontWeight: "bold",
+                    textTransform: "uppercase",
+                  }}
+                  gutterBottom
+                >
+                  {groupName}
+                </Typography>
+                <TableContainer component={Paper}>
+                  <Table sx={{ width: "100%" }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow className="table_header">
+                        <TableCell className="table_header_cell">
+                          Order Name
+                        </TableCell>
+                        <TableCell className="table_header_cell">
+                          Status
+                        </TableCell>
+                        <TableCell className="table_header_cell">
+                          Quanitity
+                        </TableCell>
+                        <TableCell className="table_header_cell">
+                          Price
                         </TableCell>
                       </TableRow>
-                    ) : null}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </CardContent>
-          </Card>
-          {Object.keys(groups).map((groupName) => {
-            return (
-              <Card
-                sx={{
-                  float: "left",
-                  margin: "1%",
-                  maxWidth: 460,
-                }}
-                key={groupName}
-              >
-                <CardContent>
-                  <Typography
-                    sx={{
-                      fontSize: 14,
-                      fontWeight: "bold",
-                      textTransform: "uppercase",
-                    }}
-                    gutterBottom
-                  >
-                    {groupName}
-                  </Typography>
-                  <TableContainer component={Paper}>
-                    <Table sx={{ maxWidth: 450 }} aria-label="simple table">
-                      <TableHead>
-                        <TableRow className="table_header">
-                          <TableCell className="table_header_cell">
-                            Order Name
-                          </TableCell>
-                          <TableCell className="table_header_cell">
-                            Status
-                          </TableCell>
-                          <TableCell className="table_header_cell">
-                            Quanitity
-                          </TableCell>
-                          <TableCell className="table_header_cell">
-                            Price
-                          </TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {groups[groupName].map((row) => (
-                          <TableRow
-                            className="table_body"
-                            key={row.name}
-                            sx={{
-                              "&:last-child td, &:last-child th": { border: 0 },
-                            }}
-                          >
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.status}</TableCell>
-                            <TableCell>{row.quantity}</TableCell>
-                            <TableCell>{row.price}</TableCell>
-                          </TableRow>
-                        ))}
+                    </TableHead>
+                    <TableBody>
+                      {groups[groupName].map((row) => (
                         <TableRow
-                          key={groupName + ": Total"}
                           className="table_body"
+                          key={row.name}
                           sx={{
                             "&:last-child td, &:last-child th": { border: 0 },
                           }}
                         >
-                          <TableCell className="table_body_last_row">
-                            Total
-                          </TableCell>
-                          <TableCell className="table_body_last_row">
-                            -
-                          </TableCell>
-                          <TableCell className="table_body_last_row">
-                            -
-                          </TableCell>
-                          <TableCell className="table_body_last_row">
-                            {groups[groupName]
-                              .reduce((a, b) => {
-                                return a + b["price"];
-                              }, 0)
-                              .toFixed(4)}
-                          </TableCell>
+                          <TableCell>{row.name}</TableCell>
+                          <TableCell>{row.status}</TableCell>
+                          <TableCell>{row.quantity}</TableCell>
+                          <TableCell>{row.price}</TableCell>
                         </TableRow>
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                </CardContent>
-              </Card>
-            );
-          })}
-        </Grid>
-      </Grid>
-    </Container>
+                      ))}
+                      <TableRow
+                        key={groupName + ": Total"}
+                        className="table_body"
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell className="table_body_last_row">
+                          Total
+                        </TableCell>
+                        <TableCell className="table_body_last_row">-</TableCell>
+                        <TableCell className="table_body_last_row">-</TableCell>
+                        <TableCell className="table_body_last_row">
+                          {groups[groupName]
+                            .reduce((a, b) => {
+                              return a + b["price"];
+                            }, 0)
+                            .toFixed(4)}
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
 
