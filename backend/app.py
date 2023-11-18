94479fe0b63cfaed71f5af05dcccd326f96ae11b
Sharan Reddy <sharansaikonda99@gmail.com>
2023-11-18 00:49:11 -0500

    parse pdf instead of randomly finding values

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from WalmartItem import WalmartItem
 from WalmartOrder import WalmartOrder
-from pdfquery import PDFQuery
 import bs4 as bs
 from flask_cors import CORS, cross_origin
+import re
+from PyPDF2 import PdfReader
+import sys
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -23,92 +25,70 @@ def upload_folder():
     file = request.files['order']
     file.save('./order.pdf')
 
-    # create xml file
-    pdf = PDFQuery('order.pdf')
-    pdf.load()
-    pdf.tree.write('order.xml', pretty_print = True)
 
-    with open('order.xml', 'r') as xmlFile:
-        contents = xmlFile.read()
-        file = bs.BeautifulSoup(contents, 'xml')
-        
-        root = file.find('LTPage')
-        root = root.find('LTRect')
-        root = root.find_all('LTRect')[1]
-        rows = root.find_all('LTTextBoxHorizontal')
-        
-        orderName = rows[0].text
-        obj = WalmartOrder(orderName)
+    # creating a pdf reader object
+    reader = PdfReader('./order.pdf')
 
-        itemKeys = []
-        itemValues = []
+    # printing number of pages in pdf file
+    for page in reader.pages:
+        # extracting text from page
+        text = page.extract_text().split("\n")
+        objectArr = []
+        object = {}
 
-        idx = 0
-
-        i = 1
-        while i < len(rows):
-            if '-$' in rows[i].text.lower():
-                i += 1
-                continue
-
-            if 'payment method' in rows[i].text.lower() or 'ending in' in rows[i].text.lower():
-                i += 1
-                continue
-
-            for key in ['Subtotal', 'Savings', 'Delivery from store', 'Tax', 'Driver tip', 'Total']:
-                if key.lower() in rows[i].text.lower():
-                    itemKeys.append(rows[i].text)
-                    i += 1
-                    break
-            else:
-                if 'qty' in rows[i].text.lower():
-                    status, qty = 'Delivered', 0
-                    if(len(rows[i].text.rstrip(" ").split(" ")) == 2):
-                        _, qty = rows[i].text.rstrip(" ").split(" ")
-                    else:
-                        status, _, qty = rows[i].text.rstrip(" ").split(" ")
-
-                    qty = float(qty)
-                    price = float(rows[i+1].text.strip(' $'))
-                    
-                    itemValues.append((status, qty, price))
-                    i += 2
+        for input_string in text:
+            result = re.search(r"^(.*?)[ ]*(unavailable|shopped|weight-adjusted|substitutions|)[ ]*Qty[ ]*([0-9]+)[ ]*\$([0-9\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                resultGroup = result.groups()
+                obj = WalmartItem(resultGroup[0])
+                obj.status = resultGroup[1]
+                obj.quantity = float(resultGroup[2])
+                obj.price = float(resultGroup[3])
+                objectArr.append(obj)
                 
-                else:
-                    if '$' in rows[i].text.strip(" ").split(" ")[-1].strip(" "):
-                        try:
-                            value = float(rows[i].text.strip(" ").split(" ")[-1][1:].strip(" "))
-                            itemValues.append(value)
-                        except Exception as e:
-                            itemKeys.append(rows[i].text.strip(" "))
-                    else:
-                        itemKeys.append(rows[i].text.strip(" "))
-                    i += 1
-            
-        idx = 0
-        for i in range(len(itemValues)):
-            key = itemKeys[i]
-            if '-$' in key.lower():
-                continue
+            result = re.search(r"order# ([\d-]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['orderName'] = result.groups()[0]
 
-            if 'subtotal' in key.lower():
-                obj.subTotal = itemValues[idx]
-            elif 'savings' in key.lower():
-                obj.subTotal = itemValues[idx]
-            elif 'delivery from store' in key.lower():
-                obj.deliveryFee = itemValues[idx]
-            elif 'tax' in key.lower():
-                obj.tax = itemValues[idx]
-            elif 'driver tip' in key.lower():
-                obj.tip = itemValues[idx]
-            elif 'total' in key.lower():
-                obj.total = itemValues[idx]
-            else:
-                item = WalmartItem(itemKeys[idx])
-                item.status = itemValues[idx][0]
-                item.quantity = itemValues[idx][1]
-                item.price = itemValues[idx][2]
-                obj.ordersArr.append(item)
-            idx += 1
+            
+            result = re.search(r"subtotal[ ]*\$([\d\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['subTotal'] = result.groups()[0]
+            
+            result = re.search(r"savings[ ]*[-]*\$([\d\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['savings'] = result.groups()[0]
+
+            
+            result = re.search(r"tax[ ]*[-]*\$([\d\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['tax'] = result.groups()[0]
+            
+            result = re.search(r"total[ ]*[-]*\$([\d\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['total'] = result.groups()[0]
+
+            result = re.search(r"driver tip[ ]*[-]*\$([\d\.]+)", input_string, re.IGNORECASE)
+            if result != None:
+                object['driverTip'] = result.groups()[0]
+            
+            result = re.search('free delivery.*?(\$[0-9\.]+)(\$[0-9\.]+)', input_string, re.IGNORECASE)
+            if result is not None:
+                val1 = result.groups()[0]
+                val2 = result.groups()[1]
+
+                if val2 != '':
+                    object['deliveryFee'] = val2.lstrip('$')
+                else:
+                    object['deliveryFee'] = val1.lstrip('$')
+    
+        obj = WalmartOrder(object['orderName'])
+        obj.subTotal = object['subTotal']
+        obj.savings = object.get('savings', 0)
+        obj.deliveryFee = object.get('deliveryFee', 0)
+        obj.tax = object.get('tax', 0)
+        obj.total = object.get('total', 0)
+        obj.tip = object.get('driverTip', 0)
+        obj.ordersArr = objectArr
         
-        return obj.toJSON()
+    return obj.toJSON()
